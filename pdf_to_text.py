2e7ca1f4ba6b12a66b363ae0de5a19589cc66757
handleDMCS <dungg1810@gmail.com>
2024-01-29 14:31:57 +0700

    Version 1 (kinda retarded) ready to deploy

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -6,51 +6,20 @@ except ImportError:
 import pytesseract
 from docx import Document
 import argparse
-import os 
-
-# sua lai pytesseract.pytesseract.tesseract_cmd
-pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-def get_path_type(fpath):
-    if os.path.isdir(fpath):
-        return 'dir'
-    elif os.path.isfile(fpath) and fpath.lower().endswith('.pdf'):
-        return 'file'
-    else:
-        return 'none'
-
-def get_docx(pdf_file):
-    # pdf -> img 
-    doc = Document()
-    # sua lai poppler_path
-    images = pdf2image.convert_from_path(pdf_path=pdf_file, poppler_path=r'C:\Release-23.11.0-0\poppler-23.11.0\Library\bin')
-    for pg, img in enumerate(images):
-        text = pytesseract.image_to_string(img, lang=args.la)
-        doc.add_paragraph(text)
-    return doc
+import os
+import docx_processing
+import path_processing
 
 def check_op():
     op = args.op
      
     if(op != ''):
-        op_ty = get_path_type(op)
+        op_ty = path_processing.get_path_type(op)
         if(op_ty != 'dir'):
             print("-op must be an existing folder !")
             return False
     
     return True
-
-def get_f_name(par, fname, ex = ''):
-    output_f = f"{par}\{fname}{ex}"
-    if os.path.exists(output_f):
-        count = 1
-        while True:
-            new_output_f = f"{par}\{fname}({count}){ex}"
-            if not os.path.exists(new_output_f):
-                output_f = new_output_f
-                break
-            count += 1
-    return output_f
 
 def handle_file():
     if(check_op() == False):
@@ -60,8 +29,8 @@ def handle_file():
     if(op == ''):
         op = os.path.dirname(ip)
 
-    doc = get_docx(ip)
-    res_path = get_f_name(par=op, fname=os.path.basename(ip)[:-4], ex='.docx') 
+    doc = docx_processing.get_docx(ip, args.la)
+    res_path = path_processing.get_f_name(par=op, fname=os.path.basename(ip)[:-4], ex='.docx') 
     print(f"Your output file is saved at : {res_path}")
     doc.save(res_path)    
 
@@ -71,16 +40,16 @@ def handle_dir():
     
     ip, op = args.ip, args.op
     if(op == ''):
-        op = get_f_name(fname=f'{os.path.basename(ip)}_to_docx', par=ip)
+        op = path_processing.get_f_name(fname=f'{os.path.basename(ip)}_to_docx', par=ip)
     else:
-        op = get_f_name(fname=f'{os.path.basename(ip)}_to_docx', par=op)
+        op = path_processing.get_f_name(fname=f'{os.path.basename(ip)}_to_docx', par=op)
     os.makedirs(op)
 
     for file in os.listdir(ip):
         path = os.path.join(ip, file)
-        if get_path_type(path) == 'file':
+        if path_processing.get_path_type(path) == 'file':
             print(file)
-            doc = get_docx(path)
+            doc = docx_processing.get_docx(path, args.la)
             doc.save(f'{op}\{file[:-4]}.docx')
     print(f'Your output files are saved at: {op}')
     
@@ -90,13 +59,10 @@ parser.add_argument('-op', type=str, default='')
 parser.add_argument('-la', choices=['eng', 'vie'], default='vie')
 args = parser.parse_args()
 
-match get_path_type(args.ip):
+match path_processing.get_path_type(args.ip):
     case 'file':
         handle_file()
     case 'dir':
         handle_dir()
     case 'none':
         print("Input must be a folder or a PDF file !")
-
-
-# get_docx(r"C:\Users\PC\Downloads\sample.pdf")
